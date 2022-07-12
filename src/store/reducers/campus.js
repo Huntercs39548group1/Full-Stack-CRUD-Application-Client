0cd6d3439224d0f3e6cf963937c1a785bb030a9c
remit233 <joeytan233@gmail.com>
2022-07-11 23:08:34 -0400

    remove delete in single campus

--- src/store/reducers/campus.js
+++ src/store/reducers/campus.js
@@ -5,7 +5,7 @@ This is a Reducer function that accepts 2 parameters: the previous state object 
 Depending on the Action object, the Reducer updates the State and return the new State object.
 It also defines the State and its default initial value.
 ================================================== */
-import {  DELETE_CAMPUS, EDIT_CAMPUS, FETCH_CAMPUS } from "../actions/actionTypes";  // Import Action Type
+import {   EDIT_CAMPUS, FETCH_CAMPUS } from "../actions/actionTypes";  // Import Action Type
 // const initialState = {
 //   current: {
 //      name: '',
@@ -27,9 +27,7 @@ const campus = (state = initialState, action) => {  // Use "initialState" as def
   switch (action.type) {
     case FETCH_CAMPUS:
       return action.payload;
-    case DELETE_CAMPUS:
-      return {...state, campus: state.campus.filter((campusId) => campusId !== action.payload)};
-    case EDIT_CAMPUS:
+ case EDIT_CAMPUS:
       return state.map(campus => {
         return ( 
           campus.id === action.payload.id ? action.payload : campus
