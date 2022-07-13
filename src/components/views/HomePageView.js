4d625b66cb6a821e0d5b659ec827867657f42dfb
remit233 <joeytan233@gmail.com>
2022-07-13 16:42:17 -0400

    Update on button

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -13,28 +13,27 @@ const HomePageView = () => {
     <div >
       <h1>Home Page</h1>
 
-      <h1 class="header">View Campuses
-      <br>
-      </br>
+      <h1 class="header">
+      
       <Link to={'/campuses'}>
-      <Button variant="contained" color="black">
-        Clink Here!
+      <Button variant="contained" color="black" size='large' fullWidth=" ">
+      View Campuses
       </Button>
+      <br></br>
+      <img src="https://img.freepik.com/free-vector/hand-draw-city-skyline-sketch_1035-19581.jpg?w=2000"width="250" height="210"/>  
     </Link>
-    <br></br><br></br>
-    <img src="https://img.freepik.com/free-vector/hand-draw-city-skyline-sketch_1035-19581.jpg?w=2000"width="150" height="100"/>
-    </h1>
+ </h1>
 
-    <h1 class="header">View Students
-    <br></br>
+    <h1 class="header">
+    
     <Link to={'/students'}>
-    <Button variant="contained" color="black">
-      Clink Here!
-    </Button>
+    <Button variant="contained" color="black" size='large' fullWidth=" ">
+    View Students    </Button>
+    <br></br><br></br>
+    <img src="https://pic.onlinewebfonts.com/svg/img_150030.png"width="150" height="100"/>
+    
   </Link>
-  <br></br><br></br>
-  <img src="https://pic.onlinewebfonts.com/svg/img_150030.png"width="150" height="100"/>
-  
+
   </h1>
     </div>
   );    
