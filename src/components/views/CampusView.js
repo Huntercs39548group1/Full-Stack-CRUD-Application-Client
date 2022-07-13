9272ad4311fb0d0f196b037330e8090fe4fd497e
dive0 <zhengshiwei5@gmail.com>
2022-07-13 12:12:27 -0500

    Bring back buttons

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -7,29 +7,51 @@ import { Link } from "react-router-dom";
 
 // Take in props data to construct the component
 const CampusView = (props) => {
-  const {campus} = props;
-  
-  // Render a single Campus view with list of its students
-  return (
-    <div>
-      <h1>{campus.name}</h1>
-      <p>{campus.address}</p>
-      <p>{campus.description}</p>
-      {
-        campus.students.map( (student) => {
-        let name = student.firstname + " " + student.lastname;
-        return (
-          <div key={student.id}>
-            <Link to={`/student/${student.id}`}>
-              <h2>{name}</h2>
-            </Link>             
-          </div>
-        );
-      })
-      }
-      
-    </div>
-  );
+    const { campus } = props;
+    const { deleteCampus } = props;
+
+    let none = () => {
+        if (campus.students.length === 0) {
+            return (
+                <div>
+                    <h3>No Students enrolled</h3>
+                </div>
+            );
+        }
+    };
+
+    // Render a single Campus view with list of its students
+    return (
+        <div>
+            <h1>{campus.name}</h1>
+            <p>{campus.address}</p>
+            <p>{campus.description}</p>
+            <div>
+                <Link to={`/campuses`}>
+                    <button onClick={() => deleteCampus(campus.id)}>
+                        Delete Campus
+                    </button>
+                </Link>
+                <Link to={`/editcampus/${campus.id}`}>
+                    <button>Edit Campus</button>
+                </Link>
+            </div>
+            {none()}
+            {campus.students.map((student) => {
+                let name = student.firstname + " " + student.lastname;
+                return (
+                    <div key={student.id}>
+                        <Link to={`/student/${student.id}`}>
+                            <h2>{name}</h2>
+                        </Link>
+                    </div>
+                );
+            })}
+            <Link to={`/newstudent`}>
+                <button>Add New Student</button>
+            </Link>
+        </div>
+    );
 };
 
 export default CampusView;
